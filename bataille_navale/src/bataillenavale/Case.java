d9f89e647d32f465438ba5b971784f506a89c806
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:47:11 +0100

    Add TODO (Case.java).

--- bataille_navale/src/bataillenavale/Case.java
+++ bataille_navale/src/bataillenavale/Case.java
@@ -1,14 +1,16 @@
+/* TODO JavaDoc. */
+/* TODO Package avec un meilleur nom. */
 package bataillenavale;
 
 public class Case{
-	private int coordonnee_x;
-	private int coordonnee_y;
-	
-	public Case (int x, int y){
-		this.coordonnee_x=x;
-		this.coordonnee_y=y;
-	}
+	/* TODO Final ? */
+    private int coordonnee_x;
+    /* TODO Final ? */
+    private int coordonnee_y;
 
-
+    public Case(int x, int y) {
+        this.coordonnee_x = x;
+        this.coordonnee_y = y;
+    }
 }
 
