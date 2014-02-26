4a511309f2bc55cd9c22f1d735057cc4cbf749b5
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:46:45 +0100

    Add TODO (Bateaux_Vedette1.java).

--- bataille_navale/src/bataillenavale/Bateaux_Vedette1.java
+++ bataille_navale/src/bataillenavale/Bateaux_Vedette1.java
@@ -1,5 +1,8 @@
+/* TODO JavaDoc. */
+/* TODO Package avec un meilleur nom. */
 package bataillenavale;
 
+/* TODO Les noms de Classe en Java n'ont pas de '_' (convention) */
 public class Bateaux_Vedette1 {
 
 }
