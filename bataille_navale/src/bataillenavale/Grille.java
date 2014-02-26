5d8bc8255761f5ca0319b24fa9022b568516a56b
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:47:20 +0100

    Add TODO (Grille.java).

--- bataille_navale/src/bataillenavale/Grille.java
+++ bataille_navale/src/bataillenavale/Grille.java
@@ -1,16 +1,18 @@
+/* TODO JavaDoc. */
+/* TODO Package avec un meilleur nom. */
 package bataillenavale;
-
-import java.util.*;
 
 public class Grille {
 	//atributs
 	/*grille avec en abscisse: les lettres en majuscule de A a J
-	 * en ordonnée les chiffres de 0 a 10, le tout sous forme de string
+	 * en ordonnï¿½e les chiffres de 0 a 10, le tout sous forme de string
 	 */
+    /* TODO Pourquoi des String ? */
 	private String[][] grille;
 
 	public Grille()
 	{
+        /* TODO En plus, Ã§a ce n'est pas des String. */.
 	grille = new String[][]{ {'A','B','C','D','E','F','G','H','I','J'},{'1','2','3','4','5','6','7','8','9','10'} };
 	
 	}
